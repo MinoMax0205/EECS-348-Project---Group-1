ad5ceb52fa9c0dae8a5fd60eab3751fc9f677309
brycemartin04 <brycemartin@ku.edu>
2024-12-08 21:14:49 -0600

    Fixed the double decimal problem

--- src/Evaluator.cpp
+++ src/Evaluator.cpp
@@ -1,5 +1,5 @@
 #include "Evaluator.hpp"
-
+#include <iostream>
 #include <stack>
 #include <string>
 #include <cmath>
@@ -32,7 +32,9 @@ double Evaluator::PerformUnaryExpression(const string &op, double operand) {
 }
 
 float Evaluator::evalexpression(const string &expression) {
-
+    for(size_t i = 0; i < expression.length(); i++){
+        if (expression[i] == '.' && expression[i+1] =='.') throw std::runtime_error("Invalid Syntax");
+    }
     stack<double> s;
     stringstream ss(expression);
     string token;
